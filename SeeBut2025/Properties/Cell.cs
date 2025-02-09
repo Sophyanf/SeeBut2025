252e42e09f50801b8d1a0763d1a642c5c5e96076
Sophya <sophyaNF@gmail.com>
2025-02-09 21:57:16 +0300

    Добавьте файлы проекта.

---
+++ SeeBut2025/Properties/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeBut2025.Resources
+{
+    internal class Cell
+    {
+        public int Num { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Value { get; set; }
+        public Cell() { }
+        public override string ToString()
+        {
+            return "arr";
+        }
+    }
+
+
+}
---
+++ SeeBut2025/Ship.cs
@@ -0,0 +1,16 @@
+﻿using SeeBut2025.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeBut2025
+{
+    internal class Ship
+    {
+        public Cell StartCell { get; set; }
+        public int ShipPozV_G { get; set; }
+        public int SizeShip { get; set; }
+    }
+}
